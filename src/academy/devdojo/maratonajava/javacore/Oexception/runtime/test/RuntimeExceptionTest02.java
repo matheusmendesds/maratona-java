2714f431ce4004c4e7e7e73c8d5756f0a527169c
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-03 14:08:20 -0300

    excecoes

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest02 {
+    public static void main(String[] args) {
+       divisao(1,0);
+
+        System.out.println("Codigo finalizado");
+    }
+    private static int divisao(int a, int b) {
+        if(b==0){
+            throw new IllegalArgumentException("Argumento ilegal, não pode ser 0");
+        }
+        return a/b;
+    }
+}
