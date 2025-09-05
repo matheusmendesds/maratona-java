2345c88a7032af4f11db7180f6bb2c8c45782f53
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-05 19:41:45 -0300

    strings

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest01.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        String nome = "Matheus";
+        String nome2 = "Matheus";
+        nome.concat("Mendes");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+        String nome3 = new String("Matheus"); //1 variavel de referencia ,2 objeto do tipo string, 3 uma string na pool de string
+
+        System.out.println(nome == nome3);
+        System.out.println(nome == nome3.intern());
+
+    }
+}
