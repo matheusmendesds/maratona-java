508bb5aaa15b8dd5d89f438dee0ad456ef48375d
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-07 13:29:42 -0300

    novo-projeto

---
+++ src/academy/devdojo/maratonajava/projetosTreino/cadastro/dominio/Veiculo.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.projetosTreino.cadastro.dominio;
+
+public class Veiculo {
+    public String tipo;
+    public String modelo;
+    public String marca;
+    public int ano;
+
+
+
+}
