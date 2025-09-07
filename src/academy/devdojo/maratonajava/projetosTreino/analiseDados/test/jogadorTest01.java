b33de09964d11f3119a2c4273bbc60fc9815d7ca
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-06 21:20:18 -0300

    commit novo

--- src/academy/devdojo/maratonajava/projetosTreino/analiseDados/test/jogadorTest01.java
+++ src/academy/devdojo/maratonajava/projetosTreino/analiseDados/test/jogadorTest01.java
@@ -7,8 +7,6 @@ public class jogadorTest01 {
         Jogador jogador01 = new Jogador("Neymar",35,10,20,24,12,200);
         // jogador01.calcularStats();
         Jogador jogador02 = new Jogador("Rollheiser",25,10,12,10,2,340);
-        // jogador02.calcularStats();
-        // jogador01.imprime();
         jogador01.analiseJogador();
         jogador02.analiseJogador();
         jogador01.imprime();
