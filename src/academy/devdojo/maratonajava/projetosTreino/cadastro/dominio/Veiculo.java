e36afa9b85333c19b13b8190b3490acf2d93a74c
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-07 13:38:43 -0300

    addveiculo

--- src/academy/devdojo/maratonajava/projetosTreino/cadastro/dominio/Veiculo.java
+++ src/academy/devdojo/maratonajava/projetosTreino/cadastro/dominio/Veiculo.java
@@ -6,6 +6,48 @@ public class Veiculo {
     public String marca;
     public int ano;
 
+    public Veiculo(String tipo, String modelo, String marca, int ano) {
+        this.tipo = tipo;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.ano = ano;
+    }
+    public void imprime(){
+        System.out.println("Tipo:" + tipo);
+        System.out.println("Modelo:" + modelo);
+        System.out.println("Marca:" + marca);
+        System.out.println("Ano:" + ano);
 
+    }
+    public String getTipo() {
+        return tipo;
+    }
 
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
 }
---
+++ src/academy/devdojo/maratonajava/projetosTreino/cadastro/test/VeiculoTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.projetosTreino.cadastro.test;
+
+import academy.devdojo.maratonajava.projetosTreino.cadastro.dominio.Veiculo;
+
+public class VeiculoTest01 {
+    public static void main(String[] args) {
+        Veiculo veiculo01 = new Veiculo("Carro","Onix","Chevrolet",2020);
+        Veiculo
+        veiculo01.imprime();
+    }
+}
