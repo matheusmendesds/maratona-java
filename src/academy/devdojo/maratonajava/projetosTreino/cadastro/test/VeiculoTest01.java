60cd69976335c14a5b5161baeb372ee184e46cfe
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-09 21:54:42 -0300

    commit

--- src/academy/devdojo/maratonajava/projetosTreino/cadastro/test/VeiculoTest01.java
+++ src/academy/devdojo/maratonajava/projetosTreino/cadastro/test/VeiculoTest01.java
@@ -6,9 +6,12 @@ public class VeiculoTest01 {
     public static void main(String[] args) {
         Veiculo veiculo01 = new Veiculo("Carro","Onix","Chevrolet",2020,24000);
         Veiculo veiculo02 = new Veiculo("Moto","Titan","Honda",2014,10000);
+        Veiculo veiculo03 = new Veiculo("Carro","Gol","Volkswagen",2025,0);
         veiculo01.imprime();
         veiculo01.addCarro();
         veiculo02.imprime();
         veiculo02.addCarro();
+        veiculo03.imprime();
+        veiculo03.addCarro();
     }
 }
