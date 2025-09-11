13cd58347de524d61e58827b3a66f5932a576730
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-11 20:22:45 -0300

    commit

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringBuilderTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringBuilderTest01.java
@@ -4,9 +4,13 @@ public class StringBuilderTest01 {
     public static void main(String[] args) {
         String nome = "Willian Suane";
         nome.concat(" DevDojo");
+        nome.substring(0,3);
         System.out.println(nome);
         StringBuilder sb = new StringBuilder("Willian Suane");
         sb.append("DevDojo");
+        sb.reverse();
+        sb.reverse();
+        sb.delete(0,3);
         System.out.println(sb);
     }
 }
