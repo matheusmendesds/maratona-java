da481b2dc5977076376d4f4f5569a840d55407b6
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-12 21:06:00 -0300

    commit

---
+++ src/academy/devdojo/maratonajava/javacore/Rdate/test/CalendarTest01.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Rdate.test;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalendarTest01 {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        if(c.getFirstDayOfWeek() == Calendar.SUNDAY){
+            System.out.println("Domingo é o primeiro dia da semana.");
+        }
+        // "pegando" dias
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+        System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+
+        // adicionando horas ou dias pelo add
+        c.add(Calendar.DAY_OF_MONTH,2);
+        c.add(Calendar.HOUR,2);
+
+        Date date = c.getTime();
+        System.out.println(date);
+    }
+}
