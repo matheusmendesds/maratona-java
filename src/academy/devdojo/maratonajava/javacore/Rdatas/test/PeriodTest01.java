08edfabf3b14f205ed854a20b80fac9b228ef5d6
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-18 21:13:13 -0300

    teste2

--- src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
@@ -1,20 +1,21 @@
 package academy.devdojo.maratonajava.javacore.Rdatas.test;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.Period;
+import java.time.Year;
 
 public class PeriodTest01 {
     public static void main(String[] args) {
         LocalDate now = LocalDate.now();
         LocalDate nowAfterTwoYears = LocalDate.now().plusYears(2).plusDays(7);
-
+        LocalDate test = now.plusYears(2);
         Period p1 = Period.between(now,nowAfterTwoYears);
         Period p2 = Period.ofDays(10);
         Period p3 = Period.ofWeeks(58);
         Period p4 = Period.ofMonths(58);
         Period p5 = Period.ofYears(3);
         Period p6 = Period.of(26,9,18);
+        System.out.println("Teste" + test);
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
