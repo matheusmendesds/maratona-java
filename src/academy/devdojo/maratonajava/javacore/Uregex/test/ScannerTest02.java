b0312b7c8eff69cb9566adcef6729f71b6e8f199
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-20 19:04:24 -0300

    scanner

---
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/ScannerTest02.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Uregex.test;
+
+import java.util.Scanner;
+
+public class ScannerTest02 {
+    public static void main(String[] args) {
+        String texto = "Levi, Eren, Mikasa";
+        Scanner scanner = new Scanner(texto);
+        scanner
+
+
+    }
+}
