ae99af1a85d1e39a127027d0daf0ad06d4c01473
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-22 20:32:50 -0300

    arquivo-form-criado

--- src/academy/devdojo/maratonajava/projetosTreino/desafioCadastro/Main.java
+++ src/academy/devdojo/maratonajava/projetosTreino/desafioCadastro/Main.java
@@ -1,7 +1,10 @@
 package academy.devdojo.maratonajava.projetosTreino.desafioCadastro;
 
+import academy.devdojo.maratonajava.projetosTreino.desafioCadastro.utils.Formulario;
+
 public class Main {
     public static void main(String[] args) {
-
+        Formulario formulario = new Formulario();
+        formulario.criarForm();
     }
 }
--- src/academy/devdojo/maratonajava/projetosTreino/desafioCadastro/utils/Formulario.java
+++ src/academy/devdojo/maratonajava/projetosTreino/desafioCadastro/utils/Formulario.java
@@ -1,4 +1,17 @@
 package academy.devdojo.maratonajava.projetosTreino.desafioCadastro.utils;
 
+import java.io.File;
+import java.io.IOException;
+
 public class Formulario {
+    public static void criarForm() {
+        try {
+            File fileFormulario= new File("C:\\Users\\masin\\OneDrive\\Documentos\\ESTUDOS\\java\\maratona-java\\src\\academy\\devdojo\\maratonajava\\projetosTreino\\desafioCadastro\\arquivos\\formulario.txt");
+            boolean isFileCreated = fileFormulario.createNewFile();
+            System.out.println(isFileCreated);
+        } catch(IOException ex){
+            ex.printStackTrace();
+        }
+    }
+
 }
