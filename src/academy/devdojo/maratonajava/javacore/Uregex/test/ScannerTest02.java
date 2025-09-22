3fd74cda78d828f6fa36fd921f7f22fef46af60d
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2025-09-22 17:37:41 -0300

    IO_file_pt1

--- src/academy/devdojo/maratonajava/javacore/Uregex/test/ScannerTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Uregex/test/ScannerTest02.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class ScannerTest02 {
     public static void main(String[] args) {
-        String texto = "Levi, Eren, Mikasa, true,200";
+        String texto = "Levi, Eren, Mikasa, true,200 ";
         Scanner scanner = new Scanner(texto);
         scanner.useDelimiter(",");
         while (scanner.hasNext()){
