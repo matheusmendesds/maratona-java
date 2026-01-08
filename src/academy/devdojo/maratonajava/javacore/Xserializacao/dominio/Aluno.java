4b5595f5d65dd7d6044e82a889b8bf5055b9cebc
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-01-08 14:52:36 -0300

    serializacao

--- src/academy/devdojo/maratonajava/javacore/Xserializacao/dominio/Aluno.java
+++ src/academy/devdojo/maratonajava/javacore/Xserializacao/dominio/Aluno.java
@@ -1,16 +1,42 @@
 package academy.devdojo.maratonajava.javacore.Xserializacao.dominio;
 
-import java.io.Serializable;
+import java.io.*;
 
 public class Aluno implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 739959230178356185L;
     private Long id;
     private String nome;
-    private String password;
+    private transient String password;
+    private final String  NOME_ESCOLA = "SantosFC";
+    private transient Turma turma;
 
     public Aluno(Long id, String nome, String password) {
+        System.out.println("Dentro do Construtor");
         this.id = id;
         this.nome = nome;
         this.password = password;
+    }
+
+    @Serial
+    private void writeObject(ObjectOutputStream oos) {
+        try {
+            oos.defaultWriteObject();
+            oos.writeUTF(turma.getNome());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Serial
+    private void readObject(ObjectInputStream ois) {
+        try {
+            ois.defaultReadObject();
+            String nomeTurma = ois.readUTF();
+            turma = new Turma(nomeTurma);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -19,7 +45,19 @@ public class Aluno implements Serializable {
                 "id=" + id +
                 ", nome='" + nome + '\'' +
                 ", password='" + password + '\'' +
+                ", NOME_ESCOLA='" + NOME_ESCOLA + '\'' +
+                ", Turma='" + turma + '\'' +
+
+
                 '}';
+    }
+
+    public Turma getTurma() {
+        return turma;
+    }
+
+    public void setTurma(Turma turma) {
+        this.turma = turma;
     }
 
     public Long getId() {
