dcaaf73c5399ec2e89eb6590b4c1ea6941e77e6d
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-01-10 14:46:06 -0300

    LinkedListatt

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/IteratorTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/IteratorTest01.java
@@ -4,11 +4,13 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class IteratorTest01 {
     public static void main(String[] args) {
-        List<Manga> mangas = new ArrayList<>(6);
+        //Usar o LinkedList é melhor para remoçao de itens na lista
+        List<Manga> mangas = new LinkedList<>();
 
         mangas.add(new Manga(5L,"Hellsing Ultimate",19.9,0));
         mangas.add(new Manga(1L,"Berserk",9.5,5));
