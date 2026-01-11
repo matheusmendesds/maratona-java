6f906616fd8866790c98be6b87e4f896656705ba
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-01-11 15:23:20 -0300

    colecao-map

---
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest01 {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+        map.put("teklado","teclado");
+        map.put("mouze","mouse");
+        map.put("vc","você");
+        map.putIfAbsent("vc","você2");
+        System.out.println(map);
+        for (String key : map.keySet()){
+            System.out.println(key +" : "+map.get(key));
+        }
+
+        System.out.println("_----------");
+
+        for (String value : map.values()){
+            System.out.println(value);
+        }
+
+        System.out.println("_____________");
+        for (Map.Entry<String, String> entry: map.entrySet()){
+            System.out.println(entry.getKey() + " - "+entry.getValue());
+        }
+    }
+}
