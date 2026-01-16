14f0fd64f8fb464cb15e2bb7d2cad8721e15a971
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-01-16 19:59:57 -0300

    classes-genericas-02

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/ClasseGenericaTest03.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+import academy.devdojo.maratonajava.javacore.Zgenerics.service.RentalService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClasseGenericaTest03 {
+    public static void main(String[] args) {
+        List<Carro> carrosDisponiveis = new ArrayList<>(List.of(new Carro("BMW"), new Carro("Mercedes"), new Carro("Fusca")));
+        List<Barco> barcosDisponiveis = new ArrayList<>(List.of(new Barco("Lancha"), new Barco("Iate"), new Barco("Canoa")));
+        RentalService<Carro> rentalServiceCarro = new RentalService<>(carrosDisponiveis);
+        Carro carro = rentalServiceCarro.buscarObjetoDisponivel();
+        System.out.println("Usando carro por um mês...");
+        rentalServiceCarro.retornarObjetoAlugado(carro);
+
+        System.out.println("----------------");
+        RentalService<Barco> rentalServiceBarco = new RentalService<>(barcosDisponiveis);
+        Barco barco = rentalServiceBarco.buscarObjetoDisponivel();
+        System.out.println("Usando barco por um mês");
+        rentalServiceBarco.retornarObjetoAlugado(barco);
+    }
+}
