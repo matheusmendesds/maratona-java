30465547c59284609112c04db2566b4b7da536d1
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-01-19 14:39:30 -0300

    comportamentos-parametrospt2

---
+++ src/academy/devdojo/maratonajava/javacore/ZZbcomportamento/interfaces/CarPredicate.java
@@ -0,0 +1,7 @@
+package academy.devdojo.maratonajava.javacore.ZZbcomportamento.interfaces;
+
+import academy.devdojo.maratonajava.javacore.ZZbcomportamento.dominio.Car;
+
+public interface CarPredicate {
+    public boolean test(Car car);
+}
