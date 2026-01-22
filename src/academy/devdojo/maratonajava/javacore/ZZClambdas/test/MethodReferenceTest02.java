a326068d161322e2310b2cec35c9029b1f53e875
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-01-21 22:08:55 -0300

    method-ref-pt3

--- src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest02.java
@@ -4,15 +4,15 @@ import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
 import academy.devdojo.maratonajava.javacore.ZZClambdas.services.AnimeComparators;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
-//Reference to a static method
-public class MethodReferenceTest01 {
+
+//Reference to an instance method of a particular object
+public class MethodReferenceTest02 {
     public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
         List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("One piece", 989), new Anime("Naruto", 143)));
-        //Collections.sort(animeList,(a1, a2) -> a1.getTitle().compareTo(a2.getTitle()));
-        //Collections.sort(animeList, AnimeComparators::compareByTitle);
-        Collections.sort(animeList,AnimeComparators::compareByEpisodes);
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
         System.out.println(animeList);
+
     }
 }
---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest04.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambdas.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.services.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+
+//Reference to a constructor
+public class MethodReferenceTest04 {
+    public static void main(String[] args) {
+        Supplier<AnimeComparators> newAnimeComparators = AnimeComparators::new;
+        AnimeComparators animeComparators = newAnimeComparators.get();
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("One piece", 989), new Anime("Naruto", 143)));
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+        System.out.println(animeList);
+
+        BiFunction<String, Integer, Anime> animeBiFunction = (title,episodes) -> new Anime(title,episodes);
+        BiFunction<String, Integer, Anime> animeBiFunction2 = Anime::new;
+        System.out.println(animeBiFunction2.apply("Super Campeos",34));
+    }
+
+}
