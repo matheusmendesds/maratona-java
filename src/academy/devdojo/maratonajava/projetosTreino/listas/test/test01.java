6939a042391d8ded1d2a75cd16ccfc4c66acf9f1
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-01-24 15:37:48 -0300

    add-times

--- src/academy/devdojo/maratonajava/projetosTreino/listas/test/test01.java
+++ src/academy/devdojo/maratonajava/projetosTreino/listas/test/test01.java
@@ -3,10 +3,20 @@ package academy.devdojo.maratonajava.projetosTreino.listas.test;
 import academy.devdojo.maratonajava.projetosTreino.listas.dominio.Jogador;
 import academy.devdojo.maratonajava.projetosTreino.listas.dominio.Time;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class test01 {
     public static void main(String[] args) {
-        Time santos = new Time("Santos FC","Brasil");
-        Jogador j1 = new Jogador("Neymar JR",33,santos);
+        Time time1 = new Time("Santos FC", "Brasil");
+        Time time2 = new Time("Real Madrid", "Espanha");
+        Time time3 = new Time("Milan", "Italia");
+        List<Time> times = new ArrayList<>();
+        times.add(time1);
+        times.add(time2);
+        times.add(time3);
+        Jogador j1 = new Jogador("Neymar JR", 33, time1);
         System.out.println(j1);
+        System.out.println(times);
     }
 }
