b1c78f2407d88f0ee1a8b50536db3af6d9c4e4fd
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-01-25 13:04:46 -0300

    criandoTimes

--- src/academy/devdojo/maratonajava/projetosTreino/listas/Main.java
+++ src/academy/devdojo/maratonajava/projetosTreino/listas/Main.java
@@ -1,14 +1,21 @@
 package academy.devdojo.maratonajava.projetosTreino.listas;
 
+import academy.devdojo.maratonajava.projetosTreino.listas.services.AddJogador;
+import academy.devdojo.maratonajava.projetosTreino.listas.services.CriarTime;
+import academy.devdojo.maratonajava.projetosTreino.listas.services.EditarDados;
 import academy.devdojo.maratonajava.projetosTreino.listas.services.PrintMenu;
 
 public class Main {
     public static void main(String[] args) {
         PrintMenu menu = new PrintMenu();
         int escolha = menu.menuPrincipal();
+        CriarTime addTime = new CriarTime();
+        AddJogador addJogador = new AddJogador();
+        EditarDados editDados = new EditarDados();
         switch (escolha){
             case 1:
                 System.out.println("Escolha 1");
+                addTime.creatingTeam();
                 break;
             case 2:
                 System.out.println("Escolha 2");
--- src/academy/devdojo/maratonajava/projetosTreino/listas/services/CriarTime.java
+++ src/academy/devdojo/maratonajava/projetosTreino/listas/services/CriarTime.java
@@ -1,4 +1,34 @@
 package academy.devdojo.maratonajava.projetosTreino.listas.services;
 
+import academy.devdojo.maratonajava.projetosTreino.listas.dominio.Time;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+//Criar lista txt com nomes dos times pra salvar e dps passar para os jogadores
+//Criar pasta de cada time com os jogadores criados la dentro
 public class CriarTime {
+     public void creatingTeam() {
+         Scanner scanner = new Scanner(System.in);
+         System.out.println("Nome do Time:");
+         String nome = scanner.nextLine();
+         System.out.println("País do Time");
+         String pais = scanner.nextLine();
+         Time novoTime = new Time(nome,pais);
+         System.out.println(novoTime);
+         saveTeam(nome,pais);
+     }
+
+     public void saveTeam(String nome ,String pais) {
+         File file = new File("C:\\Users\\masin\\OneDrive\\Documentos\\ESTUDOS\\java\\maratona-java\\src\\academy\\devdojo\\maratonajava\\projetosTreino\\listas\\data\\times\\"+nome+".txt");
+         try(FileWriter fw = new FileWriter(file,true);
+             BufferedWriter bw = new BufferedWriter(fw)) {
+             bw.write("Nome:" + nome +"\n");
+             bw.write("País:"+ pais);
+         } catch (IOException e) {
+             throw new RuntimeException(e);
+         }
+     }
 }
