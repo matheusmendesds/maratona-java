eed6e0fa994d95dab3b059539d30d040394d347a
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-02-06 20:29:15 -0300

    stream-pt3

--- src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest03.java
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest03.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamTest03 {
     private static List<LightNovel> lightNovels =new ArrayList<>( List.of(
@@ -19,6 +20,12 @@ public class StreamTest03 {
     ));
 
     public static void main(String[] args) {
-        
+        Stream<LightNovel> stream = lightNovels.stream();
+        lightNovels.forEach(System.out::println);
+        long count = stream.filter(ln -> ln.getPrice() <= 4).count();
+        long count2 = lightNovels.stream().distinct().filter(ln -> ln.getPrice() <= 4).count();
+
+        System.out.println(count);
+        System.out.println(count2);
     }
 }
