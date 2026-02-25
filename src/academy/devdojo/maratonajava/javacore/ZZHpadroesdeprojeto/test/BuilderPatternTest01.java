4069ce1b03d98f7b5ada1dd6ebe9bcf6e2e9f3f3
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-02-24 21:10:38 -0300

    padroes-projeto01

---
+++ src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/test/BuilderPatternTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.test;
+
+import academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.dominio.Person;
+
+public class BuilderPatternTest01 {
+    public static void main(String[] args) {
+        Person build = new Person.PersonBuilder()
+                .firstName("Willian")
+                .lastName("Suane")
+                .username("ViradonoJiraya")
+                .email("wiilian@gmail.com")
+                .build();
+        System.out.println(build);
+    }
+}
