e91666476f2d4cc556a1745d7c366abf82d4a635
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-02-24 21:45:19 -0300

    padroes-projeto03-singleton

---
+++ src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/dominio/Aircraft.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.dominio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class Aircraft {
+    private final Set<String> availabeSeats = new HashSet<>();
+    private final String name;
+
+    public Aircraft(String name) {
+        this.name = name;
+    }
+
+    {
+        availabeSeats.add("1A");
+        availabeSeats.add("1B");
+    }
+
+    public boolean bookSeat(String seat) {
+        return availabeSeats.remove(seat);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/javacore/ZZHpadroesdeprojeto/dominio/AircraftSingletonEager.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.ZZHpadroesdeprojeto.dominio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class AircraftSingletonEager {
+    //Eager Initialization
+    private static final AircraftSingletonEager INSTANCE = new AircraftSingletonEager("787-900");
+    private final Set<String> availabeSeats = new HashSet<>();
+    private final String name;
+
+    private AircraftSingletonEager(String name) {
+        this.name = name;
+    }
+
+    {
+        availabeSeats.add("1A");
+        availabeSeats.add("1B");
+    }
+    public static AircraftSingletonEager getINSTANCE(){
+        return INSTANCE;
+    }
+
+    public boolean bookSeat(String seat) {
+        return availabeSeats.remove(seat);
+    }
+}
