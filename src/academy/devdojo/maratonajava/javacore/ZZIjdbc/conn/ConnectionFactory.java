97b483b2d7ff98c2e88cb6b4f3ebce40f819efc4
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-02-27 18:47:31 -0300

    add-dependencias

--- src/academy/devdojo/maratonajava/javacore/ZZIjdbc/conn/ConnectionFactory.java
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/conn/ConnectionFactory.java
@@ -1,5 +1,21 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.conn;
 
-public class ConnectionFactory {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class ConnectionFactory {
+    public static Connection getConnection() {
+        String url="jdbc:mysql://localhost:3306/anime_store";
+        String userName="root";
+        String password="root";
+        try {
+            Connection connection = DriverManager.getConnection(url, userName, password);
+            System.out.println(connection);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
