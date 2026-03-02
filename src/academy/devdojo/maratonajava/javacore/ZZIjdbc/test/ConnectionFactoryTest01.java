0c4d206e5f7578d87c2e2d5152158711075a3ed3
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-03-02 16:28:06 -0300

    lombok-e-Log4j2

--- src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -1,14 +1,18 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 
-import academy.devdojo.maratonajava.javacore.ZZIjdbc.conn.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import lombok.extern.log4j.Log4j2;
 
-import java.sql.SQLException;
-
+@Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("NHK").build();
-        ProducerRepository.save(producer);
+//        Producer producer = Producer.builder().name("Studio Deen").build();
+//        ProducerRepository.save(producer);
+        log.info("info");
+        log.debug("debug");
+        log.warn("warn");
+        log.error("error");
+        log.trace("trace");
     }
 }
---
+++ target/classes/academy/devdojo/maratonajava/introducao/aulas.md
@@ -0,0 +1,3 @@
+Aula 02 - Tipos Primitivos;
+int, long, double, char, boolean, byte, short são os tipos primitivos no Java
+Aula 04- Operadores;
