d8f31879c6b3e4c868d63b388e50f5bd7b0b0712
Matheus Mendes <122118515+matheusmendesds@users.noreply.github.com>
2026-03-11 21:21:38 -0300

    RecordClass-and-patternMatchingForInstanceOf

---
+++ src/test/java/academy/devdojo/maratonajava/javacore/ZZKjunit/dominio/MangaTest.java
@@ -0,0 +1,34 @@
+package academy.devdojo.maratonajava.javacore.ZZKjunit.dominio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MangaTest {
+    private Manga manga1;
+    private Manga manga2;
+    @BeforeEach
+    public void setUp() {
+        manga1 = new Manga("Kimetsu no Yaiba",24);
+        manga2 = new Manga("Kimetsu no Yaiba",24);
+    }
+
+    @Test
+    public void acessors_ReturnData_WhemInitialized() {
+        Assertions.assertEquals("Kimetsu no Yaiba",manga1.name());
+        Assertions.assertEquals(24,manga1.episodes());
+    }
+
+    @Test
+    public void equals_ReturnTrue_WhenObjectsAreTheSame() {
+        Assertions.assertEquals(manga1,manga2);
+    }
+
+    @Test
+    public void hashCode_ReturnTrue_WhenObjectsAreTheSame() {
+        Assertions.assertEquals(manga1.hashCode(),manga2.hashCode());
+    }
+
+}
